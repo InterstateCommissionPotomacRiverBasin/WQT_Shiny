627fa060dd8249af6e93f598f36c79450470e5e4
Zachary Smith <zsmith@ICPRB.INT>
2017-09-20 14:54:52 -0400

    Made the size of the plots more dynamic.

--- ui/figures_ui.R
+++ ui/figures_ui.R
@@ -1,7 +1,7 @@
 tabPanel("Figures",
          fluidRow(
            align = "center",
-           plotOutput("PLOTS", height = "1200", width = "1500")
+           plotOutput("PLOTS", height = "1200px", width = "100%")
            #fluidRow(uiOutput("plot.ui"))
            #fluidRow(plotOutput("PLOTS", height = uiOutput("plot.height"), width = "1500"))
          ) # End fluidRow
