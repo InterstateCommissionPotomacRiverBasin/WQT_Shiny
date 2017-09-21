b94d1e1f06c6bced044d4a2d9d4c58b92bfd9621
Zachary Smith <zsmith@ICPRB.INT>
2017-09-21 10:22:23 -0400

    Updated sites to start by reflecting all TEMP sites. App loads faster becuase it prevents the app from looking for a site that does not have TEMP data.

--- global/import_data.R
+++ global/import_data.R
@@ -3,4 +3,9 @@ param.range <- dbReadTable(pool, "param_range")
 outliers <- dbReadTable(pool, "Outliers")
 huc8 <- dbReadTable(pool, "huc_8")
 list.huc <- c("All HUCs", sort(unique(huc8$HUC_8)))
-sites <- unique(huc8[order(huc8$SITE), ]$SITE)
+sites <-  dbGetQuery(pool, paste('SELECT DISTINCT "SITE"',
+                                 'FROM "wq_data"',
+                                 'WHERE "ICPRB_NAME" =', "'TEMP'",
+                                 'ORDER BY "SITE"')) %>% 
+  pull(SITE)
+RPostgreSQL::dbReadTable(pool, "param_range")
