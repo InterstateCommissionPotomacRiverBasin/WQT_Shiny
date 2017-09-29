a4864a700961b0fb2aa89ab9ce7569b803558c64
Zachary Smith <zsmith@ICPRB.INT>
2017-09-29 08:07:51 -0400

    Updated map_server and added a way to automatically update all tabs at once. However, the App is  a little bit slower, so I commented it out.

--- server.R
+++ server.R
@@ -10,6 +10,8 @@ shinyServer(function(input, output, session) {
   #----------------------------------------------------------------------------
   # Stop App when window is closed.
   session$onSessionEnded(stopApp)
+  
+
   #----------------------------------------------------------------------------
   # Selection and import script
   source("server/select_import_server.R", local = TRUE)
@@ -30,7 +32,7 @@ shinyServer(function(input, output, session) {
   source("server/dt_server.R", local = TRUE)
   #----------------------------------------------------------------------------
   # Tab Maps Script (Tab 3)
-  source("server/map_server_dev.R", local = TRUE)
+  source("server/map_server_dev2.R", local = TRUE)
   #----------------------------------------------------------------------------
   # Tab Metadata Script (Tab 4)
   source("server/metadata_server.R", local = TRUE)
@@ -40,4 +42,10 @@ shinyServer(function(input, output, session) {
   #----------------------------------------------------------------------------
   # Tab References Script (Tab 6)
   source("server/relevant_pubs_server.R", local = TRUE)
+  
+#  lapply(c("PLOTS", "param_table", #"mymap",
+           #"META_OUTLIER_INFO", "PARAM_STAND_LOOP",
+           #"SITE_INFO_LOOP", "ALL_DEPTH",
+#           "relevant_pubs_table"),
+#         function(x) outputOptions(output, x, suspendWhenHidden = FALSE))
 }) # End Shiny Server
